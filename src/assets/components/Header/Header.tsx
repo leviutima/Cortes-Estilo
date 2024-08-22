11b9f228be47d413f0af1f7c08a069571fed5d6c
Levi Utima <levi.utima@gmail.com>
2024-08-22 00:16:10 -0300

    criando estrutura de pastas

---
+++ src/assets/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import style from '../Header/Header.module.css'
+
+const Header = () => {
+    return(
+        <>
+            <header>
+                <nav>
+                    <img className={style.imgMain}></img>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header 
