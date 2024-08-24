9353afff148836bc6af23fcb291025c79f9771b6
Levi Utima <levi.utima@gmail.com>
2024-08-23 21:45:25 -0300

    integrando com backend

---
+++ src/Services/formService.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export interface FormData {
+    nome: string;
+    telefone: string;
+    email: string;
+    mensagem: string;
+}
+
+export const submitForm = async (formData: FormData): Promise<void> => {
+    try {
+        const response = await axios.post('http://localhost:8080/Usuario', formData)
+    } catch (error) {
+        console.error ('erro ao enviar dados', error)
+        throw error
+    }
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,58 +1,83 @@
 import Header from '../../components/Header/Header';
-import style from '../Home/Home.module.css'
-import imgAbout from '../../assets/young-man-barber-s-shop-getting-his-beard-trimmed.jpg'
+import style from '../Home/Home.module.css';
+import imgAbout from '../../assets/young-man-barber-s-shop-getting-his-beard-trimmed.jpg';
 import Button from '../../components/Button/Button';
 import Footer from '../../components/Footer/Footer';
+import { submitForm, FormData } from '../../Services/formService';
 
 const Home = () => {
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        event.preventDefault();
+
+        const formData: FormData = {
+            nome: (event.currentTarget.elements.namedItem('nome') as HTMLInputElement).value,
+            telefone: (event.currentTarget.elements.namedItem('telefone') as HTMLInputElement).value,
+            email: (event.currentTarget.elements.namedItem('email') as HTMLInputElement).value,
+            mensagem: (event.currentTarget.elements.namedItem('mensagem') as HTMLTextAreaElement).value,
+        };
+
+        try {
+            await submitForm(formData);
+            alert('Dados enviados com sucesso');
+        } catch (error) {
+            alert('Erro ao enviar dados, tente novamente');
+        }
+    };
+
     return (
-        <>  
-            <Header/>
+        <>
+            <Header />
             <section id="Main" className={style.containerSectionMain}>
                 <div className={style.backgroundImage}></div>
                 <div className={style.containerTitle}>
                     <h1>CORTE & ESTILO</h1>
                 </div>
             </section>
+
             <section id='About' className={style.containerSectionSecond}>
                 <div className={style.containerAbout}>
                     <div>
-                        <img src={imgAbout} className={style.aboutImg}></img>
+                        <img src={imgAbout} className={style.aboutImg} alt="Sobre nós"></img>
                     </div>
                     <div className={style.paddingText}>
-                        <h2>QUEM SOMOS?</h2>  
-                        <p>Lorem Ipsum is simply dummy text 
-                            of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard 
-                            dummy text ever since the 1500s, when an unknown 
-                            printer took a galley of type and scrambled 
-                            it to make a type specimen book. It has survived 
-                            not only five centuries, but also the leap into 
-                            electronic typesetting, remaining essentially 
-                            unchanged. It was popularised in the 1960s with 
-                            the release of Letraset sheets containing Lorem 
-                            Ipsum passages, and more recently with desktop 
-                            publishing software like Aldus PageMaker including 
-                            versions of Lorem Ipsum.</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing 
+                        <h2>QUEM SOMOS?</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing 
+                            and typesetting industry.
+                            Lorem Ipsum is simply dummy text of the printing 
                             and typesetting industry. Lorem Ipsum has been the 
                             industry's standard dummy text ever since the 1500s,
-                             when an unknown printer took a galley of type and 
-                             scrambled it to make a type specimen book. It has 
-                             survived not only five centuries, but also the leap 
-                             into electronic typesetting, remaining essentially unchanged. 
-                             It was popularised in the 1960s with the release of 
-                             Letraset sheets containing Lorem Ipsum passages, and more 
-                             recently with desktop publishing software like Aldus PageMaker 
-                             including versions of Lorem Ipsum.</p>
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the 
+                            leap into electronic typesetting, remaining 
+                            essentially unchanged. It was popularised in the 
+                            1960s with the release of Letraset sheets
+                            containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing software like 
+                            Aldus PageMaker including versions of Lorem 
+                            Ipsum.</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing 
+                                and typesetting industry. Lorem Ipsum has been the 
+                                industry's standard dummy text ever since the 1500s, 
+                                when an unknown printer took a galley of type and 
+                                scrambled it to make a type specimen book. It has 
+                                survived not only five centuries, but also the leap 
+                                into electronic typesetting, remaining essentially 
+                                unchanged. It was popularised in the 1960s with the 
+                                release of Letraset sheets containing Lorem Ipsum 
+                                passages, and more recently with desktop publishing 
+                                software like Aldus PageMaker including versions of 
+                                Lorem Ipsum.</p>
                     </div>
                 </div>
             </section>
+
             <section id='Services' className={style.containerService}>
                 <div>
                     <h2>NOSSOS SERVIÇOS</h2>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Corte de cabelo</span>
                         <span>________________________________________________________</span>
@@ -62,7 +87,7 @@ const Home = () => {
                         <span>______</span>
                     </div>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Cabelo + barba</span>
                         <span>________________________________________________________</span>
@@ -72,7 +97,7 @@ const Home = () => {
                         <span>______</span>
                     </div>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Barba</span>
                         <span>________________________________________________________</span>
@@ -80,9 +105,9 @@ const Home = () => {
                     <div className={style.precos}>
                         <span>R$ 15</span>
                         <span>______</span>
-                    </div>   
+                    </div>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Barba + sombrancelha</span>
                         <span>________________________________________________________</span>
@@ -92,7 +117,7 @@ const Home = () => {
                         <span>______</span>
                     </div>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Sombrancelha</span>
                         <span>________________________________________________________</span>
@@ -102,7 +127,7 @@ const Home = () => {
                         <span>______</span>
                     </div>
                 </div>
-                <div className= {style.subContainerService}>
+                <div className={style.subContainerService}>
                     <div className={style.service}>
                         <span>Cabelo + sombrancelha + barba</span>
                         <span>________________________________________________________</span>
@@ -113,47 +138,53 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
             <section id='Contact' className={style.containerSectionForm}>
                 <h2>CONTATO</h2>
-                <form className= {style.containerForm}>
+                <form className={style.containerForm} onSubmit={handleSubmit}>
+                    <div className= {style.subContainerForm}>
+                        <div className={style.containerInput}>
+                            <label>Nome</label>
+                            <input
+                                className={style.firstInput}
+                                type='text'
+                                name='nome'
+                                placeholder='Digite seu nome...'>
+                            </input>
+                        </div>
+                        <div className={style.containerInput}>
+                            <label>Telefone</label>
+                            <input
+                                className={style.firstInput}
+                                type='tel'
+                                name='telefone'
+                                placeholder='Digite seu telefone...'>
+                            </input>
+                        </div>
+                    </div>
                     <div className={style.containerInput}>
-                        <label>Nome</label>
-                        <input 
-                        className={style.firstInput} 
-                        type='Text' 
-                        placeholder='Digite seu nome...'>
+                        <label>Email</label>
+                        <input
+                            className={style.inputEmail}
+                            type='email'
+                            name='email'
+                            placeholder='Digite seu email...'>
                         </input>
                     </div>
                     <div className={style.containerInput}>
-                        <label>Telefone</label>
-                        <input 
-                        className={style.firstInput}
-                        type='tel'
-                        placeholder='Digite seu telefone...'
-                        ></input>
-                    </div>
-                </form>
-                <form className={style.secondInput}>
-                    <div className={style.containerInput}>
-                        <label>Email</label>
-                        <input 
-                        className={style.inputEmail}
-                        type='email'
-                        placeholder='Digite seu email...'
-                        ></input>
-                    </div>
-                    <div className={style.containerInput}>
                         <label>Mensagem</label>
-                        <textarea 
-                        className={style.inputMensagem} 
-                        placeholder='Digite sua mensagem...'></textarea>
+                        <textarea
+                            className={style.inputMensagem}
+                            name='mensagem'
+                            placeholder='Digite sua mensagem...'></textarea>
                     </div>
+                    <Button type='submit'>ENVIAR</Button>
                 </form>
-                <Button>ENVIAR</Button>
             </section>
-            <Footer/>
+
+            <Footer />
         </>
-    )
-}
+    );
+};
 
 export default Home;
